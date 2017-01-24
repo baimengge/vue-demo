8fbff5c69e088b5c018a471986414c99f0c09f23
baimengge <baimengge@rongyitech.com>
2017-01-24 11:08:49 +0800

    配置路由和模板

--- app/src/main.js
+++ app/src/main.js
@@ -1,11 +1,45 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+// 程序的入口
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import router from './router'
+import routes from './routes'
+import App from './App'
+// 导入模板
+import Hello from './components/Hello.vue'
+import Article from './components/Article.vue'
+
+// 告诉vue要使用router
+Vue.use(VueRouter)
+
+// 实例化router对象
+const router = new VueRouter({
+  mode: 'hash', // 设置路由模式 可选值: "hash" | "history" | "abstract"，默认"hash"
+  linkActiveClass: 'u-link--Active', // 这是链接激活时的class
+  routes // 挂载路由集合
 })
+
+//编写路由集合
+const routes = [
+	{
+		name: 'home', // 路由名(选填)
+		path: '/', // 根路由
+		component: Home // 模板
+	},
+	{
+		name: 'Article',
+		path: '/Article',
+		component: Article
+	}
+]
+
+// 导出router对象
+export default router
+
+new Vue({
+  // el: '#app',
+  // template: '<App/>',
+  // components: { App }
+  router,
+  render: h => h(App)
+}).$mount('#app')
